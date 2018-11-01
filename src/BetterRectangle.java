3da0fee7e92d2f876be2bce093af3b72585e5695
drwillsmith@gmail.com <Alex110602>
2018-11-01 11:15:18 -0500

    chapter 10 hints

---
+++ src/BetterRectangle.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+public class BetterRectangle extends Rectangle {
+
+    public BetterRectangle(int i, int i1, int i2, int i3) {
+        super(i, i1, i2, i3);
+    }
+
+    public double getArea(){
+        double area = 0.0;
+
+        return area;
+    }
+
+    public double getPerimeter(){
+        double perimeter = 0.0;
+
+        return perimeter;
+    }
+}
---
+++ src/RectangleChecker.java
@@ -0,0 +1,13 @@
+public class RectangleChecker {
+
+    public static void main(String arg[]) {
+
+        // Enter  your code
+        BetterRectangle one = new BetterRectangle(0,0,4,5);
+
+        one.getArea();
+
+        one.getPerimeter();
+
+    }
+}
